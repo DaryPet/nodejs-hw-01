4a78f0bb2342f12b409a5899d8c63163cf3adc1d
DaryPet <d7akkord@gmail.com>
2024-06-04 00:03:01 +0200

    1+2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { promises as fs } from 'fs';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    for (let index = 0; index < number; index++) {
+      contacts.push(createFakeContact());
+    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    console.log(`Successfully added ${number} contacts.`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await generateContacts(5);
